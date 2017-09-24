29d2fd8c5d54b23f14958b01c29515df698167b0
xiaoyingquan <xiaoyingquan1@gmail.com>
2017-09-24 09:12:59 +0800

    add i18n and beijing time strftime

--- Gemfile
+++ Gemfile
@@ -40,6 +40,8 @@ gem 'aasm'
 gem 'carrierwave-qiniu'
 gem 'qiniu-rs'
 gem 'figaro'
+gem 'rails-i18n'
+gem 'devise-i18n'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,23 @@ Bundler.require(*Rails.groups)
 
 module Jdstore
   class Application < Rails::Application
+
+    config.i18n.default_locale = "zh-CN"
+    config.time_zone = "Beijing"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
   end
 end
+
+Time::DATE_FORMATS.merge!(:default => '%Y/%m/%d %I:%M %p', :ymd => '%Y/%m/%d')
+
+datetime = Time.now
+
+datetime.to_s(:db)            # => "2007-12-04 00:00:00"
+datetime.to_s(:number)        # => "20071204000000"
+datetime.to_s(:short)         # => "04 Dec 00:00"
+datetime.to_s(:long)          # => "December 04, 2007 00:00"
+datetime.to_s(:long_ordinal)  # => "December 4th, 2007 00:00"
+datetime.to_s(:rfc822)        # => "Tue, 04 Dec 2007 00:00:00 +0000"
+datetime.to_s(:iso8601)       # => "2007-12-04T00:00:00+00:00"
